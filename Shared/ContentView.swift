e41c54ea1c71c31e9cd97246258c6c6a948b97f5
DustinOlsen <69872283+DustinOlsen@users.noreply.github.com>
2022-06-16 14:15:40 -0500

    Revamping UI and building Saved Ideas view

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,93 +12,48 @@ struct ContentView: View {
    
     var ideaDice = IdeaDice()
     @State private var ideaText = ""
+    @State private var showingSaveSheet = false
+    
+    func saveIdea() {
+        showingSaveSheet.toggle()
+    }
     
     var body: some View {
+        NavigationView {
+            
+            TabView {
+                
+                
+                DiceRollView()
+                    .tabItem {
+                        Text("Dice")
+                    }
+                
+                
+                SavedIdeasView()
+                    .tabItem {
+                        Text("Saved Ideas")
+                    }
+                
+                
+            }
+            
+            
+            
+            
+//            .toolbar {
+//                ToolbarItem(placement: .navigationBarTrailing) {
+//                    Text("Saved")
+//
+//                    // Change to NavigationLink
+//                }
+//                ToolbarItem {
+//                    Button(action: saveIdea) {
+//                        Label("Add Item", systemImage: "plus")
+//                    }
+//                }
+//            }
         
-        VStack {
-            
-            Spacer()
-            
-            Text("\(ideaText)")
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .frame(width: 350.0, height: 250.0)
-            
-            Spacer()
-            
-            Section(header: Text("Dice")) {
-                
-                HStack {
-                    
-                    VStack {
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedPower, die: .power)
-                            
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: 1)
-                            .onTapGesture {
-                                ideaText = ideaDice.reRollDie(.power)
-                            }
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedMaterial, die: .materials)
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: 1)
-                            .onTapGesture {
-                                ideaText = ideaDice.reRollDie(.materials)
-                            }
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedScale, die: .scale)
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: 1)
-                            .onTapGesture {
-                               ideaText = ideaDice.reRollDie(.scale)
-                            }
-                        
-                    }.padding()
-                    
-                    VStack {
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedConsumer, die: .consumer)
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                            .onTapGesture {
-                               ideaText = ideaDice.reRollDie(.consumer)
-                            }
-                        
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedAction, die: .action)
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                            .onTapGesture {
-                               ideaText = ideaDice.reRollDie(.action)
-                            }
-                        
-                        
-                        IdeaDiceView(ideaAspect: ideaDice.selectedDevice, die: .device)
-                            .padding(12.0)
-                            .frame(width: 115.0, height: 115.0)
-                            .border(Color.primary, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
-                            .onTapGesture {
-                               ideaText = ideaDice.reRollDie(.device)
-                            }
-                        
-                    }.padding()
-                }
-            }
-            
-            Button("Roll") {
-                ideaText = ideaDice.rollIdea()
-                
-            }
-            .buttonStyle(.bordered)
-            .padding(30)
-            
-            
-            Spacer()
         }
     }
 
@@ -109,5 +64,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
